f71169997bfaa3d61d6c6943e9e9b54d4487223b
Bamorial <107000074+Bamorial@users.noreply.github.com>
2023-09-25 15:55:38 +0300

    class binding

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,31 @@ import { ref } from 'vue';
 
 let isActive=ref(true)
 let name=ref('')
-let letters=[' ','B','A','M','O','R','I','A','L',' ','S','Z','N']
+let letters=[':',' ','T','h','i','s',' ','i','s',' ','m','y',' ','l','a','t','e','s','t',' ','m','a','n','i','f','e','s','t','a','t','i','o','n','.',' ','I',' ','w','a','n','t',' ','y','o','u',' ','t','o',' ','j','o','i','n',' ','m','y',' ','j','o','u','r','n','e','y',' ','a','n','d',' ','p','l','a','y',' ','m','y',' ','l','i','t','t','l','e',' ','g','a','m','e','s','.',' ','H','a','v','e',' ','f','u','n','!']
+let brownCol='rgb(247, 241, 232);'
+let isBrown=ref(false)
 let i=0;
 name.value='Bamorial'
 const scrolling = (e) => {
   isActive.value=false;
+  if(i<letters.length){
    name.value=name.value+letters[i]
    i++
-   if(i>letters.length-1)i=0
+   
   }
+  else{
+    isBrown.value=true;
+  }
+}
 </script>
 <template>
-  <div @wheel="scrolling"  @touchmove="scrolling" class=" h-screen w-screen flex items-center justify-center flex-col ">
-  <div class="m-2 font-primary text-[40px] text-primary">{{ name }}</div>
-  <div v-if="isActive" class=" font-mono text-primary text-[10px] -mt-2 ">SEASON 3.0 WEB ADVENTURE ISSUE 1</div>
+  <div :class="{brownCol: isBrown}" @wheel="scrolling"  @touchmove="scrolling" class=" h-screen w-screen flex items-center justify-center flex-col bodyCol">
+  <div :class="{txt:isBrown}" class="m-6 font-primary text-[40px] text-primary sm: text-[20px]">{{ name }}</div>
+  <div v-if="isActive" class=" font-mono text-primary text-[10px] -mt-6 sm: text-[10px] ">SEASON 3.0 WEB ADVENTURE ISSUE 1</div>
+  <div>
+ <img src="./assets/map.svg" alt="">
+
+  </div>
 </div>
+
 </template>
