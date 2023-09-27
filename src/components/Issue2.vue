269175e49f25f7efc0ce1ebc536804edd226664e
Bamorial <107000074+Bamorial@users.noreply.github.com>
2023-09-27 16:26:10 +0300

    issue2.1

--- src/components/Issue2.vue
+++ src/components/Issue2.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from 'vue';
 let img=ref("")
-let img2="src/assets/milk.png"
-let img1="src/assets/shake.png"
+let img2="https://i.ibb.co/1bjCD44/milk.png"
+let img1="https://i.ibb.co/BjKcFpG/milkshake.png"
+
+// "https://i.ibb.co/BjKcFpG/milkshake.png"
+
  img.value=img1
 function switchFunc(){
     if(img.value===img1){
@@ -17,5 +20,7 @@ function switchFunc(){
 <template>
     <div @click="switchFunc()" class=" w-screen h-screen md:w-auto lg:h-auto bg-black flex justify-center items-center">
         <img  :src="img"  class="border-white border border-2 md:scale-[90%]" alt="">
+       
+      
     </div>
 </template>
