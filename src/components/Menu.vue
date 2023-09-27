e778d3b9822a8fd66004541071122e993857b879
Bamorial <107000074+Bamorial@users.noreply.github.com>
2023-09-27 15:23:19 +0300

    new title text

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,19 @@
 <script setup>
 
+
 </script>
 <template>
  <div class=" h-screen w-screen bg-digitalBg">
-  <div class="font-mono text-center pt-10 text-digitalTxt">Discover</div>
+  <pre   class="font-mono text-center text-[7px] md:text-xs  pt-10 text-digitalTxt">      
+        $$\ $$\                                                                 
+      $$ |\__|                                                              
+ $$$$$$$ |$$\  $$$$$$$\  $$$$$$$\  $$$$$$\ $$\    $$\  $$$$$$\   $$$$$$\    
+$$  __$$ |$$ |$$  _____|$$  _____|$$  __$$\\$$\  $$  |$$  __$$\ $$  __$$\ 
+$$ /  $$ |$$ |\$$$$$$\  $$ /      $$ /  $$ |\$$\$$  / $$$$$$$$ |$$ |  \__|
+$$ |  $$ |$$ | \____$$\ $$ |      $$ |  $$ | \$$$  /  $$   ____|$$ |      
+\$$$$$$$ |$$ |$$$$$$$  |\$$$$$$$\ \$$$$$$  |  \$  /   \$$$$$$$\ $$ |      
+ \_______|\__|\_______/  \_______| \______/    \_/     \_______|\__|
+  </pre>
   <div class="flex flex-col">
     <RouterLink :to="{path:'Introduction'}" ontouchstart=""  class=" font-mono text-digitalTxt pl-4 pt-4 hover:text-[#95FF99] active:text-[#95FF99]  ">-->Issue #1: Introduction</RouterLink>
     
