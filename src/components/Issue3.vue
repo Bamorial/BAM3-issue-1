07efc985d203bb60b745e4abcd3ea7c70497ad92
Bamorial <107000074+Bamorial@users.noreply.github.com>
2023-10-11 18:58:11 +0300

    testing new things

--- src/components/Issue3.vue
+++ src/components/Issue3.vue
@@ -1,28 +1,38 @@
 <script setup>
 import { ref } from 'vue';
-let img=ref("")
-let imgList=["https://i.ibb.co/x6gkqqh/1.png","https://i.ibb.co/YhVrmFW/2.png","https://i.ibb.co/QbcbRT8/3.png","https://i.ibb.co/6XsnWVK/4.png","https://i.ibb.co/BCwN9Hw/5.png"]
 
-// "https://i.ibb.co/BjKcFpG/milkshake.png"
-let i=0;
- img.value=imgList[i]
-function switchFunc(){
-    if(i<imgList.length-1){
-        i++;
-      img.value=imgList[i]
 
-    }
-    else{
-        i=0;
-        img.value=imgList[i]
+let posX=ref(0)
+let posY=ref(0)
+let y=ref('')
+let x=ref('')
+let color=ref('')
+let colorSecondary=ref('')
 
-    }
+function getPos(e){
+    //console.log(" x: " + e.clientX + " y: " + e.clientY)
+     posX.value=e.clientX;
+     posY.value= e.clientY;
+     console.log(posX.value)
+     color.value="rgb("+posX.value+", "+posY.value+", 0)"
+     let a=posX.value+255
+     let b=posY.value-255
+    
+     y.value=(posY.value-20)+"px"
+     x.value=(posX.value-20)+"px"
+
+     colorSecondary.value="rgb("+a+", "+b+", 0)"
+     console.log(color.value)
+     console.log(colorSecondary.value)
+     console.log(y.value)
+
 }
+console.log(color)
 </script>
 <template>
-    <div @click="switchFunc()" class=" w-screen h-screen md:w-auto lg:h-auto bg-black flex justify-center items-center">
-        <img  :src="img"  class="border-white border border-2 md:scale-[90%]" alt="">
-       
-      
-    </div>
+<div class=" h-screen w-screen" :style="{backgroundColor: color}" @mousemove="getPos($event)">
+
+    <div class=" bg-red-100 h-10 w-10 absolute" :style="{backgroundColor: colorSecondary, top: y, left: x}">Hello</div>
+   
+</div>
 </template>
